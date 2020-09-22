89207ffa21003ec903519efe197e7d832fbff7a9
mohamedsa1990 <mohamedsa1990@gmail.com>
2020-09-22 21:33:36 +0000

    refractor spinner

--- spinner2.js
+++ spinner2.js
@@ -1,33 +1,9 @@
-// setTimeout(() => {
-//   process.stdout.write('\r|   ');
-// }, 100);
-
-// setTimeout(() => {
-//   process.stdout.write('\r/   ');
-// }, 300);
-
-// setTimeout(() => {
-//   process.stdout.write('\r-   ');
-// }, 500);
-
-// setTimeout(() => {
-//   // Need to escape the backslash since it's a special character.
-//   process.stdout.write('\r\\   '); 
-// }, 700);
-
-
-// setTimeout(() => {
-//   process.stdout.write('\r|   \n');
-// }, 900);
-
-
 // refractored code 
 
 let x = 1;
 const chars = ["|", "/", "-", "\\", "|", "\n"]
-for (char of chars) {
-  const c = char;
+for (const char of chars) {
   setTimeout(() => {
-    process.stdout.write('\r' + c);
+    process.stdout.write('\r' + char);
   }, x += 200)
 };
